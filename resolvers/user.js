c992c2125d841b6659bb0c97c5e9e90ad18ae73e
Steven Magelowitz <steven.magelowitz@gmail.com>
2018-06-24 11:19:25 -0700

    hash password with bcrypt

--- resolvers/user.js
+++ resolvers/user.js
@@ -1,9 +1,21 @@
+import bcrypt from 'bcrypt';
+
 export default {
   Query: {
     getUser: (parent, { id }, { models }) => models.User.findOne({ where: { id } }),
     allUsers: (parent, args, { models }) => models.User.findAll(),
   },
   Mutation: {
-    createUser: (parent, args, { models }) => models.User.create(args),
+    register: async (parent, { password, ...otherArgs }, { models }) => {
+      const hashedPassword = await bcrypt.hash(password, 12);
+
+      try {
+        await models.User.create({ ...otherArgs, password: hashedPassword });
+        return true;
+      } catch (e) {
+        console.log(e);
+        return false;
+      }
+    },
   },
 };
