63037f292abffbf130ce9edd244f682df8eb14b1
Steven Magelowitz <steven.magelowitz@gmail.com>
2018-06-24 16:33:42 -0700

    Add validation errors for the user

--- resolvers/user.js
+++ resolvers/user.js
@@ -1,4 +1,13 @@
 import bcrypt from 'bcrypt';
+import _ from 'lodash';
+
+const formatErrors = (e, models) => {
+  if (e instanceof models.sequelize.ValidationError) {
+    return e.errors.map(err => _.pick(err, ['path', 'message']));
+  }
+
+  return [{ path: 'name', message: 'Something went wrong' }];
+};
 
 export default {
   Query: {
@@ -10,11 +19,28 @@ export default {
       const hashedPassword = await bcrypt.hash(password, 12);
 
       try {
-        await models.User.create({ ...otherArgs, password: hashedPassword });
-        return true;
+        if (password.length < 5 || password.length > 25) {
+          return {
+            ok: false,
+            errors: [{
+              path: 'password',
+              message: 'The password needs to be between 5 and 25 characters'
+            }],
+          };
+        }
+
+        const user = await models.User.create({ ...otherArgs, password: hashedPassword });
+        console.log('stuff');
+        return {
+          ok: true,
+          user,
+        };
       } catch (e) {
         console.log(e);
-        return false;
+        return {
+          ok: false,
+          errors: formatErrors(e, models),
+        };
       }
     },
   },
